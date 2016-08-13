d1ac29ebff1b562c86c3bb6e75389dd2ad64323d
Christian Nilsson <nikize@gmail.com>
2016-08-14 00:25:23 +0200

    Make script runnable ** refatcoring needed for __name__ == 'main' usage ** use pyscreenshoot instead of PIL Add 1920x1080 resolution + fallback calculations Try and Optimize get coolant, returns int percentage   Include error check for top border of coolant   Show coolant in debug window (for now)

--- EngKnappar.py
+++ EngKnappar.py
@@ -1,10 +1,15 @@
 # Artemis Engineering Console by Stugo
-# NOTE: This script requires the following Python modules:   
+# NOTE: This script requires the following Python modules:
 #  pygame   - http://www.pygame.org/
 #  pySerial - http://pyserial.sourceforge.net/
 #  PIL - http://www.pythonware.com/products/pil/
-# Win32 users may also need:  
-#  pywin32  - http://sourceforge.net/projects/pywin32/ 
+# Win32 users may also need:
+#  pywin32  - http://sourceforge.net/projects/pywin32/
+#
+# pip install pygame
+# pip install pyserial
+# pip install pypiwin32
+# pip install pyscreenshot
 #
 # This script is based on a screen resolution of 1366x768 or 1280x800
 #
@@ -21,7 +26,7 @@ import time
 import serial
 import sys
 import win32api, win32con
-from PIL import ImageGrab
+import pyscreenshot as ImageGrab
 from win32api import GetSystemMetrics
 from pygame.locals import *
 
@@ -37,7 +42,7 @@ from pygame.locals import *
 # Slider location:
 # Y: 525 - 725
 # X: 50 + 168*n
-# Coolant: 
+# Coolant:
 # Y: 535, 745
 # X: 94 + 168*n
 # Heat:
@@ -48,90 +53,21 @@ from pygame.locals import *
 # Slider location:
 # Y: 543 - 755
 # X: 50 + 157*n
-# Coolant: 
+# Coolant:
 # Y: 555, 780
 # X: 94 + 157*n
 # Heat:
 # Y: 485, 523
 # X: 84 + 157*n
 
-print("Artemis Engineering Console by Stugo")
-print("You'll need to run Artemis in Full Screen Windowed")
-
-pygame.init()
-
-# Set the width and height of the screen [width,height]
-#size = [300, 200]
-#screen = pygame.display.set_mode(size)
-#pygame.display.set_caption("Artemis Engineering Console by Stugo")
-
-# Get the pixel width and height
-width = GetSystemMetrics (0)
-height = GetSystemMetrics (1)
-
-#Sets the coordinates
-if width == 1366 and height == 768:
-    COOLX = 94          # X coordinate to the center of the first blue coolant arrow
-    COOLXSPACE = 168    # Spacing in X between coolant systems
-    COOLYUP = 535       # Y coordinate to the center of a blue coolant up-arrow
-    COOLYDOWN = 745     # Y coordinate to the center of a blue coolant down-arrow
-    SLIDETOP = 525    # Y coordinate to the top of a energy-slider
-    SLIDEBOT = 725    # Y coordinate to the bottom of a energy-slider
-    SLIDEX = 50         # X coordinate to the center of the first energy-slider
-    SLIDESPACE = 168    # Spacing in X between energy-sliders
-    HEATTOP = 464       # Y coordinate to the top of a heatbar
-    HEATBOT = 503       # Y coordinate to the bottom of a heatbar
-    HEATX = 84          # X coordinate to the center of the first beatbar
-    HEATSPACE = 168     # Spacing in X between heatbars
-    print("1366x768 is selected")
-elif width == 1280 and height == 800:
-    COOLX = 94
-    COOLXSPACE = 157
-    COOLYUP = 555
-    COOLYDOWN = 780
-    SLIDETOP = 543
-    SLIDEBOT = 755
-    SLIDEX = 50
-    SLIDESPACE = 157
-    HEATTOP = 485
-    HEATBOT = 523
-    HEATX = 84
-    HEATSPACE = 157
-    print("1280x800 is selected")
-else:
-    print("Sorry, your screen resolution",width,"x",height, "is not supported.")
-
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-# Initialize the joysticks
-pygame.joystick.init()
-
-# Count the joysticks:
-joystickCount = pygame.joystick.get_count()
-
-if joystickCount == 0:
-    print("Sorry, no joysticks were found!")
-
-else:
-    myJoy = pygame.joystick.Joystick(0)
-    myJoy.init()
-    print("Joystick name: ", myJoy.get_name())
-    print("Joystick id: ", myJoy.get_id())
-    print("Number of buttons: ", myJoy.get_numbuttons())
-
-# open the serial port that your arduino 
-# is connected to.
-#ser = serial.Serial("COM9", 9600)
-#time.sleep(2)
-
 def click(x,y):         # Make the cursor move and click
+    print("click ", x, y)
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
 def clickset(x,y):         # Make the cursor move and click, sliders
+    print("clickset ", x, y)
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     time.sleep(1)
@@ -148,7 +84,7 @@ def coolantup(nbr):     # Make the coolant go up
     click(x,y)
 
 def coolantdown(nbr):   # Make the coolant go down
-    x = COOLX + COOLXSPACE * (nbr - 8)
+    x = COOLX + COOLXSPACE * nbr
     y = COOLYDOWN
     click(x,y)
 
@@ -160,105 +96,177 @@ def setslider(nbr, value):      # Move the slider
     #print(x, y)
 
 def getHeat():      # This bit is written by Davr. It gets the overheat-level /Modded by Stugo
-        px=ImageGrab.grab().load() # capture an image of the screen
-        color = 0
-        for i in range(0,8): # loop over all 8 heat indicators
-            x = HEATX + HEATSPACE*i # math to calculate the X position of the center of the heat indicator
-            total = 0
-            for y in range(HEATBOT, HEATTOP, -1): # loop over all the Y positions in the heat indicator
-                if sum(px[x,y]) > 200: # if the sum of red, blue, and green channels is over 200
-                    total+=1
-            b = 100 * total/(HEATBOT - HEATTOP) # calculate total heat for this heat indicator
-            if b != 100:
-                b = b // 20 + 1         # convert it to a range 1-5
-            else:
-                b = 5
-            heatList[i] = int(b)
-        #print("Heat: " + str(heatList))
+    HeatHeight = HEATBOT - HEATTOP
+    bbox=(HEATX,HEATTOP,HEATX+HEATSPACE*(8-1)+1,HEATBOT)
+    # grab interesting part of screen
+    g = ImageGrab.grab(bbox)
+    px = g.load()
+    for x in range(0, g.width):
+        for y in range(0, g.height):
+            screen.set_at((x,y), px[x,y])
+    pygame.display.flip()
+    heatList = [0]*8
+    for i in range(0,8): # loop over all 8 heat indicators
+        x = HEATSPACE*i # math to calculate the X position of the center of the heat indicator
+        total = 0.0
+        for y in range(HeatHeight-1, -1, -1): # loop over all the Y positions in the heat indicator
+            c = px[x,y]
+            #print(i,y, c, total, sum(c))
+            if c == (57, 67, 69):
+                print ("pixel has border color: ", i, (x, y), c, " Calibrate positions")
+            if sum(px[x,y]) > 200: # if the sum of red, blue, and green channels is over 200
+                total+=1
+        heatList[i] = int(100 * total/HeatHeight) # calculate total heat for this heat indicator
 
-def updateLEDs():
-    a = str(heatList).strip("[]")
-    b = a + " /n"
-    c = str.encode(b)
+    print("Heat: " + str(heatList))
+    return heatList
+
+def updateLEDs(heatList):
+    c = str.encode(str(heatList).strip("[]") + " /n")
     ser.write(c)
 
+if __name__ == '__main__':
+    print("Artemis Engineering Console by Stugo, big mod by NiKiZe")
+    print("You'll need to run Artemis in Full Screen Windowed")
+
+    # Get the pixel resolution of main screen
+    res = (GetSystemMetrics (0), GetSystemMetrics (1))
+
+    #Sets the coordinates
+    COOLX = 94          # X coordinate to the center of the first blue coolant arrow
+    SLIDEX = 50         # X coordinate to the center of the first energy-slider
+    HEATX = 84          # X coordinate to the center of the first beatbar
+    if res == (1366, 768):
+        COOLXSPACE = 168    # Spacing in X between coolant systems
+        COOLYUP = 535       # Y coordinate to the center of a blue coolant up-arrow
+        COOLYDOWN = 745     # Y coordinate to the center of a blue coolant down-arrow
+        SLIDETOP = 525    # Y coordinate to the top of a energy-slider
+        SLIDEBOT = 725    # Y coordinate to the bottom of a energy-slider
+        SLIDESPACE = COOLXSPACE    # Spacing in X between energy-sliders
+        HEATTOP = 464       # Y coordinate to the top of a heatbar
+        HEATBOT = 503       # Y coordinate to the bottom of a heatbar
+        HEATSPACE = COOLXSPACE     # Spacing in X between heatbars
+        print("%ix%i is selected" % res)
+    elif res == (1280, 800):
+        COOLXSPACE = 157
+        COOLYUP = 555
+        COOLYDOWN = 780
+        SLIDETOP = 543
+        SLIDEBOT = 755
+        SLIDESPACE = COOLXSPACE
+        HEATTOP = 485
+        HEATBOT = 523
+        HEATSPACE = COOLXSPACE
+        print("%ix%i is selected" % res)
+    elif res == (1920, 1080):
+        COOLXSPACE = 237
+        COOLYUP = 724
+        COOLYDOWN = 1059
+        SLIDETOP = 692
+        SLIDEBOT = 1050
+        SLIDESPACE = COOLXSPACE
+        HEATTOP = 652
+        HEATBOT = 693
+        HEATSPACE = COOLXSPACE
+        print("%ix%i is selected" % res)
+    else:
+        print("Screen resolution %ix%i is not tested, using scaled values." % res)
+        width, height = res
+        COOLXSPACE = int((width - 24) / 8)    # Spacing in X between coolant systems
+        COOLYUP = int(724.0 / 1080 * height)      # Y coordinate to the center of a blue coolant up-arrow
+        COOLYDOWN = int(1059.0 / 1080 * height)     # Y coordinate to the center of a blue coolant down-arrow
+        SLIDETOP = int(692.0 / 1080 * height)    # Y coordinate to the top of a energy-slider
+        SLIDEBOT = int(1050.0 / 1080 * height)    # Y coordinate to the bottom of a energy-slider
+        SLIDESPACE = COOLXSPACE    # Spacing in X between energy-sliders
+        HEATTOP = int(652.0 / 1080 * height)       # Y coordinate to the top of a heatbar
+        HEATBOT = int(693.0 / 1080 * height)       # Y coordinate to the bottom of a heatbar
+        HEATSPACE = COOLXSPACE     # Spacing in X between heatbars
+    print(COOLX, COOLXSPACE, COOLYUP, COOLYDOWN, SLIDETOP, SLIDEBOT, SLIDEX, SLIDESPACE, HEATTOP, HEATBOT, HEATX, HEATSPACE)
 
 
-heatList = [0]*8
-ctr = 0
+    pygame.init()
 
-#Loop until the user clicks the close button.
-done = False
+    # Set the width and height of the screen [width,height]
+    size = [HEATSPACE*(8-1) + 2, 200]
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Artemis Engineering Console by Stugo")
 
-# -------- Main Program Loop -----------
-while not done:
-    pygame.time.wait(1)
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
 
-    #ctr += 1
-    #if ctr > 200:
-    #    getHeat()
-    #    updateLEDs()
-    #    ctr = 0
-    
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        
-        
-        elif event.type == pygame.JOYBUTTONDOWN:
-            if event.button >= 0 and event.button <=7:
-                coolantup(event.button)
-                
-            elif event.button >= 8 and event.button <= 15:
-                coolantdown(event.button)
-                
-            elif event.button == 16:
-                press (0x31)
-                print("1")
-            elif event.button == 17:
-                press (0x32)
-                print("2")
-            elif event.button == 18:
-                press (0x33)
-                print("3")
-            elif event.button == 19:
-                press (0x34)
-                print("4")
-            elif event.button == 20:
-                press (0x35)
-                print("5")
-            elif event.button == 21:
-                press (0x36)
-                print("6")
-            elif event.button == 22:
-                press (0x37)
-                print("7")
-            elif event.button == 23:
-                press (0x38)
-                print("8")
-            elif event.button == 24:
-                press (0x39)
-                print("9")
-            elif event.button == 25:
-                press (0x30)
-                print("0")
-            elif event.button == 26:
-                press (0xA0)
-                print("Shift")
-            elif event.button == 27:
-                press (0x20)
-                print("Spacebar")
-            elif event.button == 28:
-                press (0x0D)
-                print("Enter")
+    # Initialize the joysticks
+    pygame.joystick.init()
 
-        elif event.type == pygame.JOYAXISMOTION:
-            if event.value < 1.0:
-                setslider(event.axis, event.value)
-           
+    # Count the joysticks:
+    joystickCount = pygame.joystick.get_count()
 
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit ()
+    if joystickCount == 0:
+        print("Sorry, no joysticks were found!")
+
+    else:
+        myJoy = pygame.joystick.Joystick(0)
+        myJoy.init()
+        print("Joystick name: ", myJoy.get_name())
+        print("Joystick id: ", myJoy.get_id())
+        print("Number of buttons: ", myJoy.get_numbuttons())
+
+    # open the serial port that your arduino
+    # is connected to.
+    #ser = serial.Serial("COM9", 9600)
+    #time.sleep(2)
+
+    ctr = 0
+
+    #Loop until the user clicks the close button.
+    done = False
+
+    # -------- Main Program Loop -----------
+    while not done:
+        pygame.time.wait(1)
+
+        ctr += 1
+        if ctr > 200:
+            heatList = getHeat()
+            if 'ser' in locals():
+                updateLEDs(heatList)
+            ctr = 0
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+            elif event.type == pygame.JOYBUTTONDOWN:
+                if 0 <= event.button and event.button <=7:
+                    coolantup(event.button)
+
+                elif 8 <= event.button and event.button <= 15:
+                    coolantdown(event.button - 8)
+
+                elif 16 <= event.button and event.button <= 24:
+                    num = event.button - 15
+                    press (0x30 + num)
+                    print(num)
+                elif event.button == 25:
+                    press (0x30)
+                    print("0")
+                elif event.button == 26:
+                    press (0xA0)
+                    print("Shift")
+                elif event.button == 27:
+                    press (0x20)
+                    print("Spacebar")
+                elif event.button == 28:
+                    press (0x0D)
+                    print("Enter")
+
+            elif event.type == pygame.JOYAXISMOTION:
+                if event.value < 1.0:
+                    setslider(event.axis, event.value)
+
+
+    # Close the window and quit.
+    # If you forget this line, the program will 'hang'
+    # on exit if running from IDLE.
+    pygame.quit ()
