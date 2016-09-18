4d30b7873e88a5767bf5f12631fca8d874ab92e2
Christian Nilsson <nikize@gmail.com>
2016-09-18 11:33:11 +0200

    add helper script to install python dependencies

---
+++ installs/python-deps-sinstall.cmd
@@ -0,0 +1,5 @@
+python -m pip install --upgrade pip --no-index --find-links=.
+pip install --upgrade --no-index --find-links=. pygame
+pip install --upgrade --no-index --find-links=. pyserial
+pip install --upgrade --no-index --find-links=. pyscreenshot
+pip install --upgrade --no-index --find-links=. image
