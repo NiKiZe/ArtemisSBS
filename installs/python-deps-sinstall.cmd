4aee148f374c71e7754c5566d302f7198877dbbf
Christian Nilsson <nikize@gmail.com>
2017-08-05 19:46:55 +0200

    add pywin32 dependency to python install script

--- installs/python-deps-sinstall.cmd
+++ installs/python-deps-sinstall.cmd
@@ -3,3 +3,4 @@ pip install --upgrade --no-index --find-links=. pygame
 pip install --upgrade --no-index --find-links=. pyserial
 pip install --upgrade --no-index --find-links=. pyscreenshot
 pip install --upgrade --no-index --find-links=. image
+pip install --upgrade --no-index --find-links=. pypiwin32
