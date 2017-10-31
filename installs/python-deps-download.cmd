1bedaf019e970a294bbe3317956663802cbcf738
Christian Nilsson <nikize@gmail.com>
2017-10-31 14:46:49 +0100

    update pip download script

--- installs/python-deps-download.cmd
+++ installs/python-deps-download.cmd
@@ -1,6 +1,9 @@
+pip download pip pygame pyserial pyscreenshot image pypiwin32 asyncio --only-binary=:all: --platform win32
+pip download pip pygame pyserial pyscreenshot image pypiwin32 asyncio --only-binary=:all: --platform win_amd64
 pip download pip
 pip download pygame
 pip download pyserial
 pip download pyscreenshot
 pip download image
 pip download pypiwin32
+pip download asyncio
